fadf636892f33319af023c4db214fe8d0e0c25cd
Jack Biddlecombe <jackbid@hotmail.co.uk>
2020-04-25 16:30:36 +0100

    Commented and further cleaned evolvedReinforced.py

--- src/agents/evolvedReinforced.py
+++ src/agents/evolvedReinforced.py
@@ -14,15 +14,25 @@ from agents.util import Util
 from nets import FullyConnected, Test
 
 class EvolvedReinforcedAgent():
+    """ Class for evolved reinforcement agent
 
-    def __init__(self):
+    Represents an agent that can train and make actions.
+
+    Has methods for replaying samples of the games,
+    calculating loss of moves and taking actions.
+
+    """
+    def __init__(self, debug = False, render = False):
         super().__init__()
-
+        
+        # Load a util object used for common methods
         self.util = Util()
 
+        # Create a new network
         self.net = FullyConnected(128, 10)
         self.net.float()
-
+        
+        # Load weights for the network using cuda or cpu
         if torch.cuda.is_available():
             self.device = torch.device('cuda')
             self.net.cuda()
@@ -31,56 +41,79 @@ class EvolvedReinforcedAgent():
             self.device = torch.device('cpu')
             self.net.load_state_dict(torch.load('../res/models/evolved1.pth', map_location=('cpu')))
 
-        print('device: ' + str(self.device) + '\n')    
-
         # Create a gym environment (game environment)
         self.env = gym.make('Breakout-ram-v0')
         self.env.frameskip = 0
 
         # Learning parameters
         self.sampleRate = 0.001
+        self.maxSampleLength = 20
+        self.networkParameterNoise = 0.005
+        self.deathPenalty = 100
+        self.learnReplayRate = 3
         self.criterion = nn.MSELoss()
         self.optimizer = optim.Adam(self.net.parameters())
 
-    # Add random noise to parameters
+        # Boolean for debugging statements
+        self.debug = debug
+        self.render = render
+        self.writeProgress = False
+
+
     def addParamNoise(self, sigma):
+        """Add noise to the network parameters
+        This is used for exploring new random actions.
+        """
         for param in self.net.parameters():
             noise = torch.FloatTensor(param.shape).uniform_(-sigma, sigma).to(self.device)
             param.data = torch.add(param, noise).to(self.device)
 
+
     def learnFromReplay(self, newObservations, newActions, newStates):
+        """ Learn from a successful replay
+        Given list of observations, actions and states, alter the current model.
 
-        for i in range(0, len(newStates)):
+        newObservations : list = List of observations where each observation is a list of ints in range[0, 255]
+        newActions : list = List of actions where each action is an integer from {0,1,2,3}
+        newStates : list = List of ints where each int is represents an OpenAI state than can be loaded 
 
-            # restore the previous state
-            self.env.ale.restoreState(newStates[i])
-            observation = newObservations[i]
+        Learn from replay variables by restoring the state, using the observation as input to the network,
+        and comparing the output with newActions.
+        """
 
-            # get action
-            ram = self.util.observationToTensor(observation)
-            ballY = int(observation[101])
+        if self.debug:
+            print('Learning from replay samples...')
 
-           # if ballY > 200 or ballY <= 0:
-            #    action = 1
-            #else:
-            action = self.util.tensorAction(self.net, ram)
+        for i in range(0, self.learnReplayRate):
+            for i in range(0, len(newStates)):
 
-            #if action == 1 or newActions[i] == 1:
-            #    continue
-            self.optimizer.zero_grad()
+                # restore the previous state
+                self.env.ale.restoreState(newStates[i])
+                observation = newObservations[i]
 
-            output = self.util.getScaledOutput(self.net, ram)
-            target = self.util.actionToTensor(newActions[i]).float().to(self.device)
-            loss = self.criterion(output, target).to(self.device)
-           # if action == 1 and newActions[i] == 1:
-            #print(output)
-            #print(target)
-            #print('action: ' + str(action) + ' target: ' + str(newActions[i]) + ' loss: ' + str(loss))
+                # get action
+                ram = self.util.observationToTensor(observation)
+                action = self.util.tensorAction(self.net, ram)
 
-            loss.backward()
-            self.optimizer.step()
+                self.optimizer.zero_grad()
+
+                output = self.util.getScaledOutput(self.net, ram)
+                target = self.util.actionToTensor(newActions[i]).float().to(self.device)
+                loss = self.criterion(output, target).to(self.device)
+
+                if self.debug:
+                    print('action: ' + str(action) + ' target: ' + str(newActions[i]) + ' loss: ' + str(loss))
+
+                loss.backward()
+                self.optimizer.step()
+
 
     def getParams(self):
+        """ Get the current parameters of the network
+
+        Get the current parameters of the network and return them,
+        so they can be saved and reloaded.
+        """
         params = []
 
         for param in self.net.parameters():
@@ -88,35 +121,51 @@ class EvolvedReinforcedAgent():
         
         return params
 
-    def restoreParams(self, params):
 
+    def restoreParams(self, params):
+        """ Restore network parameters
+
+        params : list = List of network parameters to be restored
+        """
         i = 0
 
         for param in self.net.parameters():
             param.data = params[i]
             i += 1
 
-    def replaySample(self, startingState, observations, actions, lives, sampleLength, originalScore):
-        
+
+    def replaySample(self, startingState, startingObservation, lives, sampleLength, originalScore):
+        """ Replay a sample of the game but randomly explore new moves
+
+        startingState : int = integer representing the state of the game to reload
+        startingObservation : list = observation to input to network at start of replay
+        lives : int = number of lives left
+        sampleLength : int = number of iterations to replay sample for
+        originalScore : int = score originally achieved by this agent (starting from the same state for the same sampleLength)
+    
+        If a greater score is achieved when replaying with exploration rate, then learn from this sample.
+        """
+
+        # Save the current network parameters and add noise to the current network
         prevParams = self.getParams()
-        self.addParamNoise(0.005)
+        self.addParamNoise(self.networkParameterNoise)
 
         # restore the starting state (since death)
         self.env.ale.restoreState(startingState)
 
+        # store new observations, actions, states, timesteps and score
         newObservations = []
         newActions = []
         states = []
-
-        didDie = False
         t = 0
         newScore = 0
 
-        observation = observations[0]
+        observation = startingObservation
         
         while True:
-
-            #self.env.render()
+            
+            if self.render:
+                self.env.render()
 
             ballY = int(observation[101])
             ram = self.util.observationToTensor(observation)
@@ -127,52 +176,55 @@ class EvolvedReinforcedAgent():
             action = self.util.tensorAction(self.net, ram)
 
             observation, reward, done, info = self.env.step(action)
-
             newActions.append(action)
 
             if t <= sampleLength:
                 newScore += reward
 
+            # Add penalty for dying
             if info['ale.lives'] < lives:
-                newScore -= 100
+                newScore -= self.deathPenalty
                 lives -= 1
 
             t += 1
-
+            
+            # If the sample is over (after buffer timesteps)
+            # If a higher score was achieved learn from replay
             if t == sampleLength + 5 or done:
                 if newScore > originalScore:
                     self.learnFromReplay(newObservations[:-5], newActions[:-5], states[:-5])
                 self.restoreParams(prevParams)
                 return
 
-        return
 
-    # One game played by the agent
-    def gameCycle(self, iteration):
+    def gameCycle(self):
+        """ One complete game 
 
-        iteration_reward = 0
-        t = 0
-        done = True
+        Simulate one complete game, start replay samples at random intervals
+
+        """
         
-        record = False
-        timeLimit = 0
-        sampleScore = 0
-        lives = 5
-        maxSampleLength = 20
-       
+        # Store observations, actions and states
         observations = []
         actions = []
         states = []
-        
+
+        # Keep track of whether to record a sample, timesteps, lives and rewards
+        record = False
+        iteration_reward = 0
+        t = 0
         sampleCounter = 0
         sampleScore = 0
+        lives = 5
 
         observation = self.env.reset()
 
         while True:
-
-            #self.env.render()
             
+            if self.render:
+                self.env.render()
+            
+            # Random chance to start a sample
             if random.uniform(0,1) < self.sampleRate:
                 record = True
 
@@ -194,18 +246,21 @@ class EvolvedReinforcedAgent():
             actions.append(action)
 
             observation, reward, done, info = self.env.step(action)
-
+            
+            # Add penalty for dying
             if info['ale.lives'] < lives:
                 sampleScore -= 100
                 lives -= 1
 
             iteration_reward += reward
+
             if record:
                 sampleScore += reward
                 sampleCounter += 1
-
-            if sampleCounter == maxSampleLength:
-                self.replaySample(states[-maxSampleLength], observations[-maxSampleLength::], actions[-maxSampleLength::], lives, maxSampleLength, sampleScore)
+            
+            # Once sample ends replay with exploration
+            if sampleCounter == self.maxSampleLength:
+                self.replaySample(states[-self.maxSampleLength], observations[-self.maxSampleLength], lives, self.maxSampleLength, sampleScore)
                 self.env.ale.restoreState(states[-1])
                 observation = observations[-1]
                 sampleScore = 0
@@ -218,19 +273,19 @@ class EvolvedReinforcedAgent():
                 return iteration_reward, t
 
 
-    # Calculate and apply loss
-    def processLoss(self, paddleMid, ballMid, output):
-        self.optimizer.zero_grad()
-
-        targetLoss = self.util.calculateTargetLoss(paddleMid, ballMid)
-        loss = self.util.applyLoss(output, targetLoss)
-        loss.backward()
-        
-        self.optimizer.step()
-
-    # At the end of training (or after fixed number of cycles) print statistics and save model
     def processTraining(self, totalReward, iterations, maxReward, elapsed_time):
+        """ Save and print training progress
 
+        At various intervals throughout training, save the model and print progress
+
+        totalReward : int = Total reward achieved over iterations number of games
+        iterations : int = Number of game iterations agent has played
+        maxReward : int = Maximum reward achieved over iterations
+        elapsed_time : int = Number of milliseconds the agent has been training for
+
+        """
+
+        # Calculate and print training statistics
         minutes = math.floor(elapsed_time/60)
         seconds = math.floor(elapsed_time - minutes * 60)
 
@@ -241,16 +296,23 @@ class EvolvedReinforcedAgent():
         print('Training took: ' + str(minutes) + ':' + str(seconds))
         print()
 
-        #file = open('../res/evolvedProgress.txt', 'a+')
-        #file.write(str(average_reward) + ' ' + str(maxReward) + '\n')
+        # Write progress to file and save model
+        if self.writeProgress:
+            file = open('../res/evolvedProgress.txt', 'a+')
+            file.write(str(average_reward) + ' ' + str(maxReward) + '\n')
         
         torch.save(self.net.state_dict(), '../res/models/evolved2.pth')
 
         maxReward = 0
         total = 0
 
-    # Train the agent
+
     def train(self, iterations):
+        """ Entry point to training the agent
+
+        itertions : int = number of game iterations to train the agent for
+
+        """
 
         maxReward = 0
         totalReward = 0
@@ -260,7 +322,7 @@ class EvolvedReinforcedAgent():
         for i_episode in range(iterations):
 
             # One complete game
-            iteration_reward, t = self.gameCycle(i_episode)
+            iteration_reward, t = self.gameCycle()
             print('Game ' + str(i_episode) + ' reward: ' + str(iteration_reward))
                 
             totalReward += iteration_reward
@@ -278,7 +340,12 @@ class EvolvedReinforcedAgent():
         self.processTraining(totalReward, iterations, maxReward, elapsed_time)
         startTime = time.time()
 
+
     def observationAction(self, observation):
+        """ Get the action from the agent given an observation
+
+        observation : list = current game state as input to network
+        """
 
         ram = self.util.observationToTensor(observation)
         output = self.util.getScaledOutput(self.net, ram)
