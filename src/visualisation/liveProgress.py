83440f0d60f1ead3976c16cf77a12ce997472c0e
Jack Biddlecombe <jackbid@Jacks-MacBook-Pro.local>
2020-04-30 18:21:52 +0100

    Got live plot to work on macos and updated gitignore

--- src/visualisation/liveProgress.py
+++ src/visualisation/liveProgress.py
@@ -8,7 +8,7 @@ from agents.supervised import SupervisedAgent
 from agents.evolvedReinforced import EvolvedReinforcedAgent
 from simulation import Simulation
 
-from multiprocessing import Process, Queue
+import multiprocessing as mp
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -19,33 +19,47 @@ ax1 = fig.add_subplot(1,1,1)
 
 
 def liveProgress(agentName, iterations, scoresQueue):
+    """ Simulate an agent 
+
+    agentName : string = name of agent to run
+    iterations : int = number if iterations to run agent
+    scoresQueus : queue = queue to put scores in
+    """
     scores = []
     os.chdir('..')
     sim = Simulation(agentName, render=True, debug=False)
 
-    for i in range(iterations):
+    # Simulate one game at a time so scores queue can be updated
+    for _ in range(iterations):
         scores.append(sim.run(1)[0])
         scoresQueue.put(scores)
-        print(str(agentName) + ' scores: ' + str(scores))
 
-def animate(i, trivialScoresQueue, reinforcedScoresQueue):
+
+def animate(frame, trivialScoresQueue, reinforcedScoresQueue):
+    """ animate is called repeatedly and used to render a live graph
+    
+    frame : int = which number frame the animation is on
+    trivialScoresQueue : queue = queue containing list of trivial agent scores
+    reinforcedScoresQueue : queue = queue containing list of reinforced agent scores
+    """
+
+    # If no data just skip animation cycle
+    # This happens when the agents have finished and the graph is still being rendered
+    if trivialScoresQueue.empty() or reinforcedScoresQueue.empty():
+        return
+
     x1, x2, y1, y2 = [], [], [], []
 
-    trivialScores = trivialScoresQueue.get()
-    reinforcedScores = reinforcedScoresQueue.get()
-
-    for k in range(len(trivialScores)):
-        y1.append(trivialScores[k])
-
-    for k in range(len(reinforcedScores)):
-        y2.append(reinforcedScores[k])
-
-    maxNumberScores = max(trivialScores, reinforcedScores)
+    # Get the scores and add them to y axis
+    y1 = trivialScoresQueue.get()
+    y2 = reinforcedScoresQueue.get()
     
-    for k in range(len(maxNumberScores)):
-        x1.append(k+1)
-        x2.append(k+1)
+    # Create x axis
+    for i in range(len(y1)):
+        x1.append(i+1)
+        x2.append(i+1)
 
+    # Plot the graph
     ax1.clear()
     ax1.plot(x1, y1, label='Trivial Agent')
     ax1.plot(x2, y2, label='Reinforcement Learning Agent')
@@ -54,17 +68,38 @@ def animate(i, trivialScoresQueue, reinforcedScoresQueue):
     plt.ylabel('Score')
 
 def startPlot(trivialScoresQueue, reinforcedScoresQueue):
+    """ Start plotting the graph
+
+    trivialScoresQueue : queue = queue containing list of trivial agent scores
+    reinforcedScoresQueue : queue = queue containing list of reinforced agent scores
+    """
     ani = animation.FuncAnimation(fig, animate, interval=1000, fargs=(trivialScoresQueue, reinforcedScoresQueue))
     plt.show()
 
+
 if __name__ == '__main__':
-    trivialScoresQueue = Queue()
-    reinforcedScoresQueue = Queue()
+    mp.set_start_method('spawn')
 
-    supervisedLiveProgressProcess = Process(target=liveProgress, args=('trivial', 15, trivialScoresQueue))
-    reinforcedScoresProgressProcess = Process(target=liveProgress, args=('evolvedreinforced', 15, reinforcedScoresQueue))
-    plot = Process(target=startPlot, args=(trivialScoresQueue, reinforcedScoresQueue))
+    # Create queue for sharing data between processes
+    trivialScoresQueue = mp.Queue()
+    reinforcedScoresQueue = mp.Queue()
 
-    supervisedLiveProgressProcess.start()
+    # Create processes for trivial agent, reinforced agent and live plot
+    trivialLiveProgressProcess = mp.Process(target=liveProgress, args=('trivial', 20, trivialScoresQueue))
+    reinforcedScoresProgressProcess = mp.Process(target=liveProgress, args=('evolvedreinforced', 20, reinforcedScoresQueue))
+    plot = mp.Process(target=startPlot, args=(trivialScoresQueue, reinforcedScoresQueue))
+
+    # Start processes
+    trivialLiveProgressProcess.start()
     reinforcedScoresProgressProcess.start()
     plot.start()
+
+    # Join processes
+    trivialLiveProgressProcess.join()
+    reinforcedScoresProgressProcess.join()
+    plot.join()
+
+    # Close processes
+    trivialLiveProgressProcess.close()
+    reinforcedScoresProgressProcess.close()
+    plot.close()
